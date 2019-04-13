e39856cc3d4de5a406f79de8d34c1f40ae7b6907
clara <10088152+claraj@users.noreply.github.com>
2019-04-13 15:13:25 -0500

    revert

--- client/src/components/LaptopList.vue
+++ client/src/components/LaptopList.vue
@@ -1,11 +1,34 @@
 <template>
   <div class="laptop-list">
 
+  
   <h2>Laptops</h2>
 
-  <!-- TODO button to create new laptop -->
+  <router-link to="/create_laptop">
+    <button class="btn btn-secondary mt-2 mb-2">Create new laptop</button>
+  </router-link>
 
-  <!-- TODO Laptop table -->
+  <table class="table table-hover">
+    <tr><th>id</th><th>Brand</th><th>Model</th><th>Serial Number</th> <th>Employee ID</th> <th>Edit</th></tr>
+    <tr v-for="laptop in laptops" v-bind:key="laptop.id">
+        <td>{{ laptop.id }}</td>
+        <td>{{ laptop.brand }}</td>
+        <td>{{ laptop.model }}</td>
+        <td>{{ laptop.serialNumber }}</td>
+        <td>
+          <router-link v-if="laptop.employeeId" :to="{ name: 'employee', params: {id: laptop.employeeId } }"> 
+            {{ laptop.employeeId }}
+          </router-link>
+        </td>
+        
+        <td>
+            <router-link :to="{ name: 'laptop', params: { id: laptop.id } }">
+                <img src="@/assets/edit.png">
+            </router-link>
+        </td>
+        
+    </tr>
+  </table>
   
   </div>
 </template>
@@ -20,10 +43,13 @@ export default {
       }
   },
   mounted() {
-    // TODO load all laptops 
+      this.$services.laptops.getAllLaptops().then( data => {
+          this.laptops = data
+      })
   }
 }
 </script>
 
 <style scoped>
 </style>
+
